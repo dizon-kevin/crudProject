27e7efffab079be8eedf9982658e709988f0f8ea
dizon-kevin <ashleynicolegutierrez7@gmail.com>
2026-02-20 22:42:05 +0800

    fix: correct order of product detail display

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -22,7 +22,7 @@
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Details:</strong>
-                {{ $product->details ?? $product->detail }}
+                {{ $product->detail ?? $product->details }}
             </div>
         </div>
     </div>
